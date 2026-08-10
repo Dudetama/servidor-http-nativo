f96a165c9f50f6342f67d3d29d18a324972a05fd
dude <motaeduarda141@gmail.com>
2026-08-10 21:19:30 +0000

    servidor criado com sucesso!

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('node:http');
+
+const PORTA = 3000
+
+const server = http.createServer((req, res) => {
+    console.log(`Requisição recebida! ${req.method} ${req.url}`);
+
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/plain; charset=utf-8')
+
+    res.end("Servidor nativo funcionando!");
+});
+
+http.Server.listen(PORTA, () => {
+    console.log(`Servidor funcionando na porta ${PORTA}`);
+});
